21a1e6ba96592c299653e31ffca3ff3cc4bce740
BishalAshiq <bishaltheboss20@gmail.com>
2021-10-01 16:53:24 +0600

    ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Rating } from '@mui/material';
+import Newspaper from './Components/Newspaper/Newspaper';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained">My first Button</Button>
+      <Rating name="read-only" value={4} readOnly/>
+      <Newspaper></Newspaper>
     </div>
   );
 }
